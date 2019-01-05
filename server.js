5433db33768d65789cd4a8ca687cbdb87d6caf63
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-05 12:13:18 -0500

    Added CRUD

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const express = require('express');
 const port = 5000;
 
 const server = express();
+server.use(express.json());
 
 server.get('/hobbits', (req, res) => {
   res.send('Welcome!');
+});
+
+// Create data
+server.post('/hobbits', (req, res) => {
+  res.status(201).json({ url: '/hobbits', operation: 'POST' });
+});
+
+// Update data
+server.put('/hobbits', (req, res) => {
+  res.status(200).json({ url: '/hobbits', operation: 'PUT' });
+});
+
+// Delete data
+server.delete('/hobbits', (req, res) => {
+  res.status(204);
 });
 
 server.listen(port, () => {
