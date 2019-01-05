fe11caf42c537bd1ff7cbce64190bcca5f758598
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-05 13:31:32 -0500

    Updated CRUD

--- server.js
+++ server.js
@@ -5,23 +5,79 @@ const port = 5000;
 const server = express();
 server.use(express.json());
 
+// Get data
 server.get('/hobbits', (req, res) => {
-  res.send('Welcome!');
+  console.log(req.query);
+  // query string parameters get added to req.query
+  const sortField = req.query.sortby || 'id';
+  const hobbits = [
+    {
+      id: 1,
+      name: 'Samwise Gamgee'
+    },
+    {
+      id: 2,
+      name: 'Frodo Baggins'
+    }
+  ];
+
+  // apply the sorting
+  const response = hobbits.sort((a, b) =>
+    a[sortField] < b[sortField] ? -1 : 1
+  );
+
+  res.status(200).json(response);
 });
 
 // Create data
+let hobbits = [
+  {
+    id: 1,
+    name: 'Bilbo Baggins',
+    age: 111
+  },
+  {
+    id: 2,
+    name: 'Frodo Baggins',
+    age: 33
+  }
+];
+let nextId = 3;
+
+// and modify the post endpoint like so:
 server.post('/hobbits', (req, res) => {
-  res.status(201).json({ url: '/hobbits', operation: 'POST' });
+  console.log(req.body);
+  const hobbit = req.body;
+  hobbit.id = nextId++;
+
+  hobbits.push(hobbit);
+
+  res.status(201).json(hobbits);
 });
 
 // Update data
-server.put('/hobbits', (req, res) => {
-  res.status(200).json({ url: '/hobbits', operation: 'PUT' });
+server.put('/hobbits/:id', (req, res) => {
+  const hobbit = hobbits.find(h => h.id == req.params.id);
+
+  if (!hobbit) {
+    res.status(404).json({ message: 'Hobbit does not exist' });
+  } else {
+    // modify the existing hobbit
+    Object.assign(hobbit, req.body);
+
+    res.status(200).json(hobbit);
+  }
 });
 
 // Delete data
-server.delete('/hobbits', (req, res) => {
-  res.status(204);
+server.delete('/hobbits/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+  // or we could destructure it like so: const { id } = req.params;
+  res.status(200).json({
+    url: `/hobbits/${id}`,
+    operation: `DELETE for hobbit with id ${id}`
+  });
 });
 
 server.listen(port, () => {
